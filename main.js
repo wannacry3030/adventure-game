0a26c1a01f73f22137e5fe4fab2bc3f25da6c469
wannacry3030 <wannacry.bat@proton.me>
2023-04-12 12:56:20 -0300

    att

--- main.js
+++ main.js
@@ -37,46 +37,58 @@ function selectOption(event) {
       // update progress and story
       progress += 25;
       updateProgress();
-      updateStory("You chose option 1. This is the end of the game.");
+      updateStory(
+        "Você escolheu a opção 1. Infelizmente, você é pego pela polícia enquanto tentava hackear um caixa eletrônico. Fim de jogo."
+      );
       updateOptions([]);
       break;
     case "option-2":
       // update progress and story
       progress += 50;
       updateProgress();
-      updateStory("You chose option 2. This is the end of the game.");
+      updateStory(
+        "Você escolheu a opção 2. Você se junta a uma gangue de rua e começa a realizar trabalhos sujos para eles. Fim de jogo."
+      );
       updateOptions([]);
       break;
     case "option-3":
       // update progress and story
       progress += 75;
       updateProgress();
-      updateStory("You chose option 3. Choose your next step:");
-      updateOptions(["Option 3-1", "Option 3-2", "Option 3-3"]);
+      updateStory(
+        "Você escolheu a opção 3. Você decide usar suas habilidades de hackeamento para invadir a base de dados de uma megacorporação e roubar informações valiosas. Mas antes de começar, você precisa escolher o tipo de ferramentas que vai usar:"
+      );
+      updateOptions([
+        "Ferramentas de hackeamento convencionais",
+        "Ferramentas de hackeamento avançadas",
+        "Ferramentas de hackeamento experimental",
+      ]);
       break;
     case "option-3-1":
       // update progress and story
       progress += 10;
       updateProgress();
-      updateStory("You chose option 3-1. This is the end of the game.");
+      updateStory(
+        "Você escolheu as ferramentas de hackeamento convencionais. Você consegue invadir a base de dados, mas as informações que encontra não são tão valiosas quanto você esperava. Fim de jogo."
+      );
       updateOptions([]);
       break;
     case "option-3-2":
       // update progress and story
       progress += 20;
       updateProgress();
-      updateStory("You chose option 3-2. This is the end of the game.");
+      updateStory(
+        "Você escolheu as ferramentas de hackeamento avançadas. Com elas, você consegue invadir a base de dados e encontrar informações valiosas. Você as vende para um grupo de ativistas que estão lutando contra as megacorporações. Fim de jogo."
+      );
       updateOptions([]);
       break;
     case "option-3-3":
       // update progress and story
       progress += 30;
       updateProgress();
-      updateStory("You chose option 3-3. This is the end of the game.");
-      updateOptions([]);
-      break;
-    default:
-      break;
+      updateStory(
+        "Você escolheu as ferramentas de hackeamento experimental. Elas funcionam bem, mas você é descoberto pela equipe de segurança da megacorporação."
+      );
   }
 }
 
