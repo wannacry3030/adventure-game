2feb0a3399753b462cd410b4bb9bdb989deecb9f
wannacry3030 <wannacry.bat@proton.me>
2023-04-12 17:10:28 -0300

    att

--- main.js
+++ main.js
@@ -109,10 +109,10 @@ function selectOption(event) {
 // start the game
 updateProgress();
 updateStory(
-  "Bem-vindo ao mundo cyberpunk de Gama City. Luzes brilhantes de neon iluminam as ruas escuras e sujas, anunciando produtos de alta tecnologia e serviços ilegais. A cidade é controlada por megacorporações que detêm todo o poder e riqueza, enquanto a maioria dos cidadãos vive em condições precárias. Você acorda em um quarto sujo e mal iluminado, sem lembrar de como chegou lá. Olhando em volta, você vê apenas uma janela pequena e uma porta trancada. Um bilhete deixado sobre a mesa diz: 'Você é um hacker. Seu próximo trabalho é recuperar dados corporativos altamente confidenciais da megacorporação GenoTech. Seja rápido e discreto, e você será recompensado generosamente. Mas cuidado, as consequências de falhar são graves'. Você se levanta, sentindo o peso da responsabilidade sobre seus ombros. Você tem uma missão a cumprir e pouco tempo para fazê-lo. O que você vai fazer agora?"
+  "Bem-vindo ao mundo cyberpunk de Gama City. Luzes brilhantes de neon iluminam as ruas escuras e sujas, anunciando produtos de alta tecnologia e serviços ilegais. A cidade é controlada por megacorporações que detêm todo o poder e riqueza, enquanto a maioria dos cidadãos vive em condições precárias. Você acorda em um quarto sujo e mal iluminado, sem lembrar de como chegou lá. Olhando em volta, você vê apenas uma janela pequena e uma porta trancada. Um bilhete deixado sobre a mesa diz: 'Você é um hacker. Seu próximo trabalho é recuperar dados corporativos altamente confidenciais da megacorporação SayonaraTech. Seja rápido e discreto, e você será recompensado generosamente. Mas cuidado, as consequências de falhar são graves'. Você se levanta, sentindo o peso da responsabilidade sobre seus ombros. Você tem uma missão a cumprir e pouco tempo para fazê-lo. O que você vai fazer agora?"
 );
 updateOptions([
   "Tentar encontrar uma saída do quarto",
-  "Investigar GenoTech",
+  "Investigar SayonaraTech",
   "Contatar um aliado",
 ]);
